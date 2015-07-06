b6787c3edb785ccb54a798ebabba90a940b3cde1
ma1co <ma1co@users.noreply.github.com>
2015-07-06 21:46:12 +0200

    Log Android boot & exit

---
+++ app/src/main/java/com/github/ma1co/pmcademo/app/BootCompletedReceiver.java
@@ -0,0 +1,12 @@
+package com.github.ma1co.pmcademo.app;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BootCompletedReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Logger.info("Boot completed");
+    }
+}
--- app/src/main/java/com/github/ma1co/pmcademo/app/ExitCompletedReceiver.java
+++ app/src/main/java/com/github/ma1co/pmcademo/app/ExitCompletedReceiver.java
@@ -7,6 +7,8 @@ import android.content.Intent;
 public class ExitCompletedReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
+        Logger.info("Exit completed");
+
         // KILL_MYSELF_TO_RELEASE_WEBVIEW_DB:
         // Kill process to release WebView when app is terminated (this hack is used by SNSDirect, too)
         android.os.Process.killProcess(android.os.Process.myPid());
